3c0755105eac855824a2657ed5f15b48d6637778
scottbringe <scott.bringe@nsssfl.com>
2015-04-03 14:11:46 -0500

    Initial Commit

---
+++ PhoneDialar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhoneDialar
+//
+//  Created by Owner on 4/3/15.
+//  Copyright (c) 2015 Scott Bringe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
